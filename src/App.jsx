5c6dc8e0ab6f57efde626b42ec0aabdf7af179d4
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-09-29 22:18:25 +0500

    not finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useContext } from 'react'
-import './App.css'
-import Dashboard from './routes/DashboardRoutes'
-import LoginRoutes from './routes/LoginRoutes'
-import { Context } from './context/Index'
+import React, { useContext, useState } from 'react';
+import './App.css';
+import Dashboard from './routes/DashboardRoutes';
+import LoginRoutes from './routes/LoginRoutes';
+import { Context } from './context/Index';
+import Feed from './pages/Feed'; // Feed komponentini import qilish
 
 function App() {
-  const {token} = useContext(Context)
-  if(token) {
-    return <Dashboard/>
-  }
-  else{
-    return <LoginRoutes/>
+  const { token } = useContext(Context);
+  const [tweets, setTweets] = useState([]); // Tweetlar uchun holat
+
+  const addTweet = (newTweet) => {
+    setTweets((prevTweets) => [...prevTweets, newTweet]); // Yangi tweetni qo'shish
+  };
+
+  if (token) {
+    return (
+      <Dashboard>
+        <Feed addTweet={addTweet} tweets={tweets} /> {/* Feed komponentini chaqirish */}
+      </Dashboard>
+    );
+  } else {
+    return <LoginRoutes />;
   }
 }
 
-export default App
+export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Login from "./Login";
 import Register from "./Register";
 import Profile from "./Profile"
-import Home from "./Home";
+import Home from "./Feed";
 
 
 export {Login,Register,Profile,Home}
